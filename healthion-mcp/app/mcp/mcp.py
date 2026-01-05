a999aee6460ce4ac2f5473a670e5f917c6d7d07a
Kacper Gąsior <kacper.gasior@themomentum.ai>
2026-01-05 12:35:04 +0100

    chore: removed healthion-mcp

--- healthion-mcp/app/mcp/mcp.py
+++
@@ -1,8 +0,0 @@
-from fastmcp import FastMCP
-
-from app.mcp.tools import heart_rate, workouts
-
-mcp_router = FastMCP(name="Main MCP")
-
-mcp_router.mount(heart_rate.heart_rate_router)
-mcp_router.mount(workouts.workouts_router)
